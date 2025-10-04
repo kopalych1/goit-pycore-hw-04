efb80c5a8defb831f656edb7b43b6f9fc4362e59
kopalych <kopalychmail@gmail.com>
2025-10-04 06:54:44 +0200

    ex04

---
+++ ex04.py
@@ -0,0 +1,79 @@
+from datetime import datetime
+from datetime import timedelta
+
+
+def get_upcoming_birthdays(users: list[dict[str:str]]):
+    """
+    Returns users whose birthdays are within the next 7 days.
+
+    :param users: List of user dictionaries with "name" and "birthday" keys.
+                  The birthday must be in the "YYYY.MM.DD" format.
+    :type users: list[dict[str, str]]
+
+    :return: List of dictionaries containing each user's name and the date
+             when they should be congratulated (in "YYYY.MM.DD" format).
+    :rtype: list[dict[str, str]]
+
+    :raises ValueError: If data types are invalid, required keys are missing,
+                        or birthday format is incorrect.
+
+    :note: Birthdays falling on weekends are moved to the following Monday.
+    """
+
+    if not len(users):
+        return []
+
+    try:
+        if any(
+            [
+                (type(user["name"]) != str or type(user["birthday"]) != str)
+                for user in users
+            ]
+        ):
+            raise ValueError("Incorrect type of data")
+    except KeyError:
+        raise ValueError("Name or birthday not found in one of users")
+
+    today = datetime.today().date()
+
+    try:
+        bdays = [
+            datetime.strptime(user["birthday"], "%Y.%m.%d")
+            .date()
+            .replace(year=today.year)
+            for user in users
+        ]
+    except ValueError as e:
+        raise e
+
+    congrat_dates = [
+        (
+            bday
+            if (bday - today) >= timedelta(days=0)
+            and (bday - today) <= timedelta(days=7)
+            else None
+        )
+        for bday in bdays
+    ]
+
+    SATURDAY = 5
+    congrat_dates = [
+        (
+            date + timedelta(days=7 - date.weekday())
+            if date and date.weekday() >= SATURDAY
+            else date
+        )
+        for date in congrat_dates
+    ]
+
+    ret = []
+    for user, date in zip(users, congrat_dates):
+        if date:
+            ret.append(
+                {
+                    "name": user["name"],
+                    "congratulation_date": date.strftime("%Y.%m.%d"),
+                }
+            )
+
+    return ret
